0b8f2d9c299322ab0737be7aaa659e7a35367f56
Joy Okwudire <okwudirejoy@gmail.com>
2021-12-18 04:40:22 +0100

    fetched data for new and upcoming games

--- src/api.js
+++ src/api.js
@@ -34,4 +34,13 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 //Popular Games
 const popular_games = `games?key=${process.env.REACT_APP_GAMES_API}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 
+//Upcoming Games
+const upcoming_games = `games?key=${process.env.REACT_APP_GAMES_API}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+
+//New Games
+const newGames = `games?key=${process.env.REACT_APP_GAMES_API}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
+
--- src/store/reducers/gamesReducer.js
+++ src/store/reducers/gamesReducer.js
@@ -1,16 +1,18 @@
 const initState = {
   popular: [],
   newGames: [],
-  upComing: [],
+  upcoming: [],
   searched: [],
 };
 
 const gamesReducer = (state = initState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return {
+      return { 
         ...state,
         popular: action.payload.popular,
+        newGames: action.payload.newGames,
+        upcoming: action.payload.upcoming,
       };
     default:
       return { ...state };
