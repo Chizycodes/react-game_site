e63c125e64bbdac518e5ea7bca040f98d15511d1
Joy Okwudire <okwudirejoy@gmail.com>
2021-12-18 03:44:47 +0100

    fetched popular games from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./store/actions/gamesAction";
+
 function App() {
-  return (
-    <div className="App">
-      Hello Ignite
-    </div>
-  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  });
+  return <div className="App">Hello Ignite</div>;
 }
 
 export default App;
--- src/store/reducers/gamesReducer.js
+++ src/store/reducers/gamesReducer.js
@@ -1,18 +1,20 @@
 const initState = {
-    popular = [],
-    newGames = [],
-    upComing = []
-}
+  popular: [],
+  newGames: [],
+  upComing: [],
+  searched: [],
+};
 
-const gamesReducer = (state=initState, action) => {
-    switch(action.type) {
-        case "FETCH_GAMES":
-            return {
-                ...state
-            }
-        default:
-            return {...state}
-    }
-}
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return {
+        ...state,
+        popular: action.payload.popular,
+      };
+    default:
+      return { ...state };
+  }
+};
 
 export default gamesReducer;
