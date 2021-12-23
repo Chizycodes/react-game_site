156babc12b1ae400f413bb72c373b1ddef51c906
Joy Okwudire <okwudirejoy@gmail.com>
2021-12-23 11:12:57 +0100

    created nav bar with search input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
 import GlobalStyles from "./components/GlobalStyles";
+import Nav from "./components/Nav";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game/:id" element={<Home />} />
